9a2b1d7c28b9ccdf4be84a2bb2fa8c69812e1991
Uchchash346 <mru416@gmail.com>
2021-05-19 16:18:41 +0600

    login page

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,7 +4,7 @@ import CheckoutProduct from './CheckoutProduct';
 import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal';
 function Checkout() {
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
 
     return (
         <div className="checkout">
@@ -14,6 +14,7 @@ function Checkout() {
                     alt=""
                 />
                 <div>
+                    <h3>Hello, {user?.email}</h3>
                     <h2 className="checkout_title">Your shopping busket</h2>
                     {
                         basket.map(item => (
